3fc7da791120ae5f07fddd96fa53bf566b838140
stjk2513 <stjk2513@gmail.com>
2025-11-11 22:14:34 -0500

    highlight overlap detector

--- src/lib/highlight.test.ts
+++ src/lib/highlight.test.ts
@@ -1,4 +1,4 @@
-import { slice } from "./highlight.ts";
+import { slice, addHighlight, isOverlap } from "./highlight.ts";
 
 test("when given string and a list of ranges with a single range, it should return three slices of text", () => {
   const text = "onetwothreefour";
@@ -32,3 +32,99 @@ test("when given multiple ranges, it should slice appropriately", () => {
 
   expect(actual).toEqual(expected);
 });
+
+test("when there is a number match, it is an overlap", () => {
+  const highlight1 = {
+    start: 1,
+    end: 3,
+  };
+  const highlight2 = {
+    start: 3,
+    end: 6,
+  };
+
+  expect(isOverlap(highlight1, highlight2)).toBe(true);
+});
+
+test("when the h1 end is bigger than h2 start, it is an overlap", () => {
+  const highlight1 = {
+    start: 1,
+    end: 5,
+  };
+  const highlight2 = {
+    start: 4,
+    end: 6,
+  };
+
+  expect(isOverlap(highlight1, highlight2)).toBe(true);
+});
+
+/*
+test("given a list of highlights, a new highlight should be added", () => {
+  let highlights = [
+    { start: 1, end: 3 },
+    {
+      start: 3,
+      end: 6,
+    },
+  ];
+  let newHighlight = { start: 6, end: 9 };
+
+  const expected = [
+    { start: 1, end: 3 },
+    { start: 3, end: 6 },
+    { start: 6, end: 9 },
+  ];
+  let newHighlights = addHighlight(highlights, newHighlight);
+  expect(newHighlights).toStrictEqual(expected);
+});
+
+test("given a list of highlights, when a new highlight is given, there should be no duplicates", () => {
+  let highlights = [
+    { start: 1, end: 3 },
+    { start: 3, end: 6 },
+    { start: 6, end: 9 },
+  ];
+  let newHighlight = {
+    start: 6,
+    end: 9,
+  };
+
+  let newHighlights = addHighlight(highlights, newHighlight);
+  expect(newHighlights).toStrictEqual(highlights);
+});
+
+test("given two highlights, it should recognize overlaps", () => {
+  const highlight1 = {
+    start: 1,
+    end: 3,
+  };
+  const highlight2 = {
+    start: 2,
+    end: 6,
+  };
+
+  expect(isOverlap(highlight1, highlight2)).toBe(true);
+});
+
+/*test("given a list of highlights, an overlapping highlight should be combined", () => {
+  let highlights = [
+    { start: 1, end: 3 },
+    { start: 3, end: 6 },
+    { start: 9, end: 12 },
+  ];
+  let newHighlight = {
+    start: 4,
+    end: 9,
+  };
+
+  const expected = [
+    { start: 1, end: 3 },
+    { start: 3, end: 9 },
+    { start: 9, end: 12 },
+  ];
+
+  let newHighlights = addHighlight(highlights, newHighlight);
+  expect(newHighlights).toStrictEqual(expected);
+});
+*/
--- src/lib/highlight.ts
+++ src/lib/highlight.ts
@@ -1,9 +1,6 @@
-export type Range = {
-  start: number;
-  end: number;
-};
+import { type Highlight } from "../store/highlights";
 
-export function slice(text: string, ranges: Range[]) {
+export function slice(text: string, ranges: Highlight[]) {
   let indents = Array.from(
     new Set([
       0,
@@ -21,4 +18,41 @@ export function slice(text: string, ranges: Range[]) {
   }
 
   return words;
+}
+
+export function addHighlight(
+  highlights: Highlight[],
+  highlight: Highlight
+): Highlight[] {
+  const isDuplicate = highlights.some((current) => {
+    return current.start === highlight.start && current.end === highlight.end;
+  });
+
+  return isDuplicate ? highlights : [...highlights, highlight];
+}
+
+export function isOverlap(
+  highlight1: Highlight,
+  highlight2: Highlight
+): boolean {
+  let common = new Set([
+    highlight1.start,
+    highlight1.end,
+    highlight2.start,
+    highlight2.end,
+  ]);
+
+  if (common.size < 4) return true;
+
+  let first;
+  let second;
+  if (highlight1.start < highlight2.start) {
+    first = highlight1;
+    second = highlight2;
+  } else {
+    first = highlight2;
+    second = highlight1;
+  }
+
+  return first.end > second.start;
 }
