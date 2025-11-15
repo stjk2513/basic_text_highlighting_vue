c024426fb83df2335c6b294e513ba4d0327f5841
stjk2513 <stjk2513@gmail.com>
2025-11-14 20:44:36 -0500

    a little structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
 import HighlightInputContainer from "./components/HighlightInputContainer.vue";
+import TextBox from "./components/TextBox.vue";
 </script>
 
 <template>
-  <HighlightInputContainer />
+  <div class="basic">
+    <div class="text-box">
+      <TextBox text="hello" />
+    </div>
+    <div class="highlight-input"><HighlightInputContainer /></div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.basic {
+  display: flex;
+}
+
+.text-box {
+  flex: 1;
+}
+</style>
