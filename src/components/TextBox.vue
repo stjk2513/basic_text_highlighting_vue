5d309f573b827c186e062009ec271d4e8c25e44f
stjk2513 <stjk2513@gmail.com>
2025-11-16 10:22:23 -0500

    render whitespace

--- src/components/TextBox.vue
+++ src/components/TextBox.vue
@@ -3,5 +3,11 @@ defineProps<{ text: string }>();
 </script>
 
 <template>
-  <div data-test="text-box">{{ text }}</div>
+  <div class="text-box" data-test="text-box">{{ text }}</div>
 </template>
+
+<style scoped>
+.text-box {
+  white-space: pre-wrap;
+}
+</style>
